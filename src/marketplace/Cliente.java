143ba19674e3b834e40da5ed275bc122e0213850
Guilherme Endres Rech <g.endres36@gmail.com>
2025-05-03 21:00:28 -0300

    Adicionando lógica de classes Cliente e dependências

--- src/marketplace/Cliente.java
+++ src/marketplace/Cliente.java
@@ -1,5 +1,37 @@
 package marketplace;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cliente extends Usuario {
+	private String cartaoCredito;
+	private List<Pedido> pedidos;
+	
+	public Cliente(String cartaoCredito) {
+		super();
+		this.cartaoCredito = cartaoCredito;
+		this.pedidos = new ArrayList<>();
+	}
+	
+	public void addPedido(Pedido pedido) {
+		pedidos.add(pedido);
+	}
+	
+	public Pedido getPedido(int numero) {
+		for(Pedido pedido : pedidos) {
+			if(pedido != null && pedido.getNumero() == numero) {
+				return pedido;
+			}
+		}
+		return null;
+	}
+
+	public String getCartaoCredito() {
+		return cartaoCredito;
+	}
+
+	public void setCartaoCredito(String cartaoCredito) {
+		this.cartaoCredito = cartaoCredito;
+	}
+	
 }
