7e232a0e4ca298df0f751201e83e97df6b907cb7
Guilherme Endres Rech <g.endres36@gmail.com>
2025-05-17 17:42:43 -0300

    loja atributes

--- src/marketplace/Loja.java
+++ src/marketplace/Loja.java
@@ -1,5 +1,16 @@
 package marketplace;
 
-public class Loja {
+import java.util.List;
+import java.util.ArrayList;
 
+public class Loja {
+    private List<Cliente> clientes;
+    private List<Fornecedor> fornecedores;
+    private Estoque estoque;
+
+    public Loja() {
+        this.clientes = new ArrayList<>();
+        this.fornecedores = new ArrayList<>();
+        this.estoque = new Estoque();
+    }
 }
